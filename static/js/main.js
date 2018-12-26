86d34ad088af05c289fa016f445f497b0d8eccd4
gabe hollander <gabehollander@gmail.com>
2018-12-26 17:51:43 -0500

    and then there was light

--- static/js/main.js
+++ static/js/main.js
@@ -18,7 +18,9 @@ var game = new Phaser.Game(
 // Preload assets
 function preload() {
 	var dir = 'static/img/assets/';
-	game.load.image('ship', dir + 'playerShip1_red.png');
+	game.load.spritesheet('ship', dir + 'star-ship.png',64,64);
+	game.load.spritesheet('enemy_1', dir + 'enemy_1.png',64,64);
+	game.load.spritesheet('enemy_1_explosion', dir + 'enemy_1_explosion.png',64,64);
 	game.load.image('laser', dir + 'laserBlue02.png');
 }
 
@@ -26,14 +28,22 @@ function preload() {
 function init() {
 	// Listen to space & enter keys
 	var keys = [Phaser.KeyCode.SPACEBAR, Phaser.KeyCode.ENTER];
+	var mkeys = [Phaser.KeyCode.LEFT, Phaser.KeyCode.RIGHT];
 	// Create Phaser.Key objects for listening to the state
 	phaserKeys = game.input.keyboard.addKeys(keys);
+	movementKeys = game.input.keyboard.addKeys(mkeys);
 	// Capture these keys to stop the browser from receiving this event
 	game.input.keyboard.addKeyCapture(keys);
+	game.input.keyboard.addKeyCapture(mkeys);
 }
 
 // Assets are available in create
 function create() {
+		
+	aliens = game.add.group();
+  aliens.enableBody = true;
+  aliens.physicsBodyType = Phaser.Physics.ARCADE;
+	createAliens();
 
 	// Create the group using the group factory
 	lasers = game.add.group();
@@ -49,15 +59,22 @@ function create() {
 
 	*/
 	lasers.createMultiple(20, 'laser');
+	
+	//  An explosion pool
+    explosions = game.add.group();
+    explosions.createMultiple(30, 'enemy_1_explosion');
+    explosions.forEach(setupInvader, this);
 
 	/*
 		Create a ship using the sprite factory
 		game.add is an instance of Phaser.GameObjectFactory, and helps us to quickly create common game objects.
 		The sprite is already added to the stage
 	*/
-	ship = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');
+	ship = game.add.sprite(game.world.centerX, game.world.height, 'ship');
+	ship.animations.add('fly', [ 0, 1, 2, 3 ], 12, true);
+	ship.play('fly');
 	// Set the anchorpoint to the middle
-	ship.anchor.setTo(0.5, 0.5);
+	ship.anchor.setTo(.5,1);
 
 	/*
 
@@ -75,12 +92,72 @@ function create() {
 	lasers.setAll('checkWorldBounds', true);
 }
 
+function createAliens () {
+
+    for (var y = 0; y < 4; y++)
+    {
+        for (var x = 0; x < 8; x++)
+        {
+            var enemy = aliens.create(x * 48, y * 50, 'enemy_1');
+            enemy.anchor.setTo(0.5, 0.5);
+            enemy.animations.add('fly', [ 0, 1, 2, 3 ], 6, true);
+            enemy.play('fly');
+            enemy.body.moves = false;
+        }
+    }
+
+    aliens.x = 20;
+    aliens.y = 50;
+
+    //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
+    var tween = game.add.tween(aliens).to( { x: 90 }, 1000, Phaser.Easing.Linear.None, true, 0, -1, true);
+		var tween = game.add.tween(aliens).to( { y: 400 }, 5000, Phaser.Easing.Linear.None, true, 0, -1, true);
+
+    //  When the tween loops it calls descend
+    tween.onLoop.add(descend, this);
+}
+function setupInvader (invader) {
+
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animations.add('enemy_1_explosion');
+
+}
+
+function descend() {
+
+    aliens.y += 10;
+
+}
+
 function resetLaser(laser) {
 	laser.kill();
 }
 
 // Update
 function update() {
+	
+	for (var idx in movementKeys) {
+		var key = movementKeys[idx];
+		// game.debug.text(Object.keys(key)[12],10, 80);
+		// game.debug.text(Object.keys(key),10, 80);
+		// game.debug.text(key.isLeft,10, 80);
+		// game.debug.text(JSON.stringify(key.keyCode),10, 80);
+		// game.debug.text(JSON.stringify(ship.x),10, 100);
+
+		if (key.isDown) {
+			if(key.keyCode === 37){
+				if (ship.x > 50) {
+					ship.x -= 5;
+				}
+			}
+			if(key.keyCode === 39){
+				if (ship.x < 400) {;
+					ship.x += 5;
+				}
+			}
+		}
+	}
 
 	// Loop over the keys
 	for (var index in phaserKeys) {
@@ -103,6 +180,8 @@ function update() {
 			touchUp();
 		}
 	}
+	
+	game.physics.arcade.overlap(lasers, aliens, collisionHandler, null, this);
 
 }
 
@@ -129,8 +208,24 @@ function fireLaser() {
 
 }
 
+function collisionHandler (bullet, alien) {
+
+    //  When a bullet hits an alien we kill them both
+    bullet.kill();
+    alien.kill();
+
+    //  And create an explosion :)
+    var explosion = explosions.getFirstExists(false);
+    explosion.reset(alien.body.x+32, alien.body.y+32);
+		explosion.anchor.x = 0.5;
+    explosion.anchor.y = 0.5;
+    explosion.play('enemy_1_explosion', 30, false, true);
+
+
+}
+
 // Render some debug text on screen
 function render() {
-	game.debug.text('CodeCaptain Shooting Demo', 10, 30);
-	game.debug.text('Click or press space / enter to shoot', 10, 55);
+	
 }
+
